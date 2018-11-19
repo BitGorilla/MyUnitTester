6ed02a66b56fdf8daf16744a51fb00fd1f9fc9f4
Martin <martinsjolund@hotmail.com>
2018-11-19 15:36:46 +0100

    Added test class without methods.

---
+++ src/Test5.java
@@ -0,0 +1,11 @@
+import se.umu.cs.unittest.TestClass;
+
+public class Test5 implements TestClass {
+
+    private MyString myString;
+
+    public Test5(){
+        myString = new MyString("Hello world!");
+    }
+
+}
